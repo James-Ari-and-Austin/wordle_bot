577da2d4d1e85a3f8d0264483b1a35eed6fdfa1c
James-Randall-14 <jammerand14@gmail.com>
2022-02-12 23:19:43 -0800

    Added a function to compare guess and answer in wordle.py

--- wordle.py
+++ wordle.py
@@ -11,7 +11,31 @@ guess_words_csv = csv.reader(file)
 answer_words_list = []
 guess_words_list = []
 for row in answer_words_csv:
-    answer_words_list.append(str(row))
-    guess_words_list.append(str(row))
+    word = str(row)[2:-2]
+    answer_words_list.append(word)
+    guess_words_list.append(word)
 for row in guess_words_csv:
-    guess_words_list.append(str(row))
+    word = str(row)[2:-2]
+    guess_words_list.append(word)
+
+#Determine words
+answer = answer_words_list[random.randrange(len(answer_words_list))]
+guess = input("Enter your guess: ")
+
+#Function Definition
+def compare(word):
+    hits = []
+    for i in range(len(word)):
+        j = 0
+        print("cycle: {0} {1} {2}".format(i, word[i], answer[i] ))
+        if word[i] == answer[i]:
+            j = j + 1
+            print("hit")
+        if word[i] in answer:
+            j = j + 1
+        hits.append(j)
+    return hits
+
+print(compare(guess))
+print(answer)
+print(guess)
