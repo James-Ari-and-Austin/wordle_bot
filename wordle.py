4ac4366df56b6ac401ca490277c0c759d3a9b64e
James-Randall-14 <jammerand14@gmail.com>
2022-02-22 00:08:44 -0800

    Combined the image creation with wordle.py

--- wordle.py
+++ wordle.py
@@ -1,7 +1,7 @@
 import csv
 import random
 import string
-from termcolor import colored
+from PIL import Image, ImageShow
 
 #Open the Word Lists
 file = open("word_libraries/answer_words.csv")
@@ -24,6 +24,17 @@ for row in guess_words_csv:
 answer = answer_words_list[random.randrange(len(answer_words_list))].upper()
 
 #Function Definition
+def createLetterImg(color, letter):
+    letterImg = Image.open("Images/Tiles/Wordle {0}/{1}.jpeg".format(color, letter))
+    letterImg = letterImg.resize((77,76))
+    return letterImg
+
+def addLetter(column, row, img, letterImg):
+    x = (column - 1) * 87 + 38
+    y = (row - 1) * 87 + 43 + round((row * 0.55))
+    img.paste(letterImg, (x,y))
+    return img
+
 def compare(word):
     checkWord = list(answer)
     guess = list(word)
@@ -56,22 +67,25 @@ def getGuess():
             end = True
     return guess
 
-def returnGuess(hits, guess):
+def returnGuess(hits, guess, cycle, img):
     for i in range(len(hits)):
         if hits[i] == 2:
-            print(colored(guess[i], 'grey', 'on_green'), end = "")
+            letterImg = createLetterImg("Green", guess[i])
+
         elif hits[i] == 1:
-            print(colored(guess[i], 'grey', 'on_yellow'), end = "")
+            letterImg = createLetterImg("Yellow", guess[i])
         else:
-            print(guess[i], end = "")
-    print("", end = "\n")
+            letterImg = createLetterImg("Gray", guess[i])
+        img = addLetter(i + 1, cycle + 1, img, letterImg)
+    ImageShow.show(img)
 
 
 def main():
+    img = Image.open("Images/WordleTemplate.jpeg")
     for cycle in range(5):
         guess = getGuess()
         hits = compare(guess)
-        returnGuess(hits, guess)
+        returnGuess(hits, guess, cycle, img)
         if checkWin(hits) == True:
             print("You got the wordle!")
             exit()
